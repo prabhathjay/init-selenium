e732105cce5dee0dbdb7f85a8686d9aca59c36dc
Prabhath <Prabhath@DESKTOP-TM4M2P9>
2023-11-06 23:17:38 +0530

    Removed unused code

--- SeleniumJF/src/test/java/testPackage/TopjobsNavigate.java
+++ SeleniumJF/src/test/java/testPackage/TopjobsNavigate.java
@@ -1,26 +1,21 @@
 package testPackage;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import java.io.File;
+
 import java.time.Duration;
 
-import org.apache.commons.io.FileUtils;
 
 public class TopjobsNavigate {
 	private WebDriver driver;
-//	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
 	
 
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
-//		System.setProperty("webdriver.chrome.driver", "");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
@@ -47,36 +42,4 @@ public class TopjobsNavigate {
 		}
 	}
 
-//	private boolean isElementPresent(By by) {
-//		try {
-//			driver.findElement(by);
-//			return true;
-//		} catch (NoSuchElementException e) {
-//			return false;
-//		}
-//	}
-//
-//	private boolean isAlertPresent() {
-//		try {
-//			driver.switchTo().alert();
-//			return true;
-//		} catch (NoAlertPresentException e) {
-//			return false;
-//		}
-//	}
-//
-//	private String closeAlertAndGetItsText() {
-//		try {
-//			Alert alert = driver.switchTo().alert();
-//			String alertText = alert.getText();
-//			if (acceptNextAlert) {
-//				alert.accept();
-//			} else {
-//				alert.dismiss();
-//			}
-//			return alertText;
-//		} finally {
-//			acceptNextAlert = true;
-//		}
-//	}
 }
